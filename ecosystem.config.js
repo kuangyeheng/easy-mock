a0619dc2a78a9a1f2f2f3a48ed1ca4b9b6f51693
kuangyeheng <kuangyeheng@gmail.com>
2018-08-23 15:29:05 +0800

    add ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [{
+    name: "easy-mock",
+    script: "./app.js",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }]
+}
